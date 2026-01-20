8e3fe94aefc20697542c2c449f6fdad235a03e77
epqlffltm <kjh55984249@gmail.com>
2026-01-20 23:06:08 +0900

    test

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import os
 from time import strftime, localtime
 
 app = Flask(__name__)
-px = Picarx()
+px = Picarx() #
 
 # 카메라 초기화
 try:
-    Vilib.camera_start(vflip=False, hflip=False)
-    Vilib.display(local=False, web=True)
+    Vilib.camera_start(vflip=False, hflip=False) #
+    Vilib.display(local=False, web=True) #
 except:
     pass
 
-# 카메라 각도 상태
+# 카메라 각도 상태 저장 변수
 current_pan = 0
 current_tilt = 0
 
@@ -33,11 +33,10 @@ def move():
         px.set_dir_servo_angle(0)
         px.backward(50)
     elif cmd == 'left':
-        px.set_dir_servo_angle(-35) # 왼쪽으로 꺾기
+        px.set_dir_servo_angle(-35)
         px.forward(40)
     elif cmd == 'right':
-        px.set_dir_servo_angle(35)  # 오른쪽으로 꺾기
-        px.forward(40)
+        px.set_dir_servo_angle(35)
     elif cmd == 'stop':
         px.stop()
     return "OK"
@@ -46,32 +45,41 @@ def move():
 def camera_control():
     global current_pan, current_tilt
     cmd = request.args.get('cmd')
-    step = 10
+    step = 10 # 한 번 누를 때 움직일 각도
     
-    # [방향 교정]
-    if cmd == 'up': current_tilt -= step      # 위로 (반대면 += 로 수정)
-    elif cmd == 'down': current_tilt += step  # 아래로
-    elif cmd == 'left': current_pan += step   # 왼쪽
-    elif cmd == 'right': current_pan -= step  # 오른쪽
-    elif cmd == 'center': current_pan, current_tilt = 0, 0
+    # [방향 교정 완료] 좌우 방향을 반대로 수정했습니다.
+    if cmd == 'up': 
+        current_tilt -= step
+    elif cmd == 'down': 
+        current_tilt += step
+    elif cmd == 'left': 
+        current_pan += step
+    elif cmd == 'right': 
+        current_pan -= step
+    elif cmd == 'center': 
+        current_pan, current_tilt = 0, 0
 
+    # 각도 제한 (-35 ~ 35도)
     current_pan = max(min(current_pan, 35), -35)
     current_tilt = max(min(current_tilt, 35), -35)
     
-    px.set_cam_pan_angle(current_pan)
-    px.set_cam_tilt_angle(current_tilt)
+    px.set_cam_pan_angle(current_pan) #
+    px.set_cam_tilt_angle(current_tilt) #
     return "OK"
 
 @app.route('/record')
 def record():
     status = request.args.get('status')
     if status == 'start':
-        Vilib.rec_video_set["name"] = strftime("%Y-%m-%d-%H.%M.%S", localtime())
-        Vilib.rec_video_run()
-        Vilib.rec_video_start()
+        Vilib.rec_video_set["name"] = strftime("%Y-%m-%d-%H.%M.%S", localtime()) #
+        Vilib.rec_video_run() #
+        Vilib.rec_video_start() #
     else:
-        Vilib.rec_video_stop()
+        Vilib.rec_video_stop() #
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    try:
+        app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    finally:
+        px.stop() #
