497c1abbe86125b1c4d3ff1fbe67b63518118bcb
epqlffltm <kjh55984249@gmail.com>
2026-01-20 21:40:42 +0900

    test

--- camera.py
+++ camera.py
@@ -1,13 +1,17 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
-if camera.isOpened():
-    ret, frame = camera.read()
-    if ret:
-        print("✅ 카메라 정상 작동!")
-        cv2.imwrite('test.jpg', frame)
+# 0, 1, 2번을 순서대로 테스트
+for i in range(3):
+    camera = cv2.VideoCapture(i)
+    if camera.isOpened():
+        ret, frame = camera.read()
+        if ret:
+            print(f"✅ {i}번 카메라에서 영상 읽기 성공!")
+            cv2.imwrite(f'test_{i}.jpg', frame)
+            camera.release()
+            break
+        else:
+            print(f"❓ {i}번 장치 열기 성공했으나 프레임 읽기 실패")
     else:
-        print("❌ 프레임 읽기 실패")
-else:
-    print("❌ 카메라 열기 실패")
-camera.release()
+        print(f"❌ {i}번 장치 열기 실패")
+    camera.release()
