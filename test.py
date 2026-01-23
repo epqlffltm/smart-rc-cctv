10326d16d7e41de1b9c824292a2b3702ed061910
epqlffltm <kjh55984249@gmail.com>
2026-01-23 16:35:08 +0900

    자동 주행 수정

--- test.py
+++ test.py
@@ -1,26 +1,12 @@
-import RPi.GPIO as GPIO
+from picarx import Picarx
 import time
 
-# Robot Hat의 Ultrasonic 핀 번호를 직접 지정 (보통 BCM 24, 25 등 보드 확인 필요)
-TRIG = 24 
-ECHO = 25
+px = Picarx()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-def get_dist():
-    GPIO.output(TRIG, False)
-    time.sleep(0.000002)
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    
-    while GPIO.input(ECHO) == 0:
-        start = time.time()
-    while GPIO.input(ECHO) == 1:
-        stop = time.time()
-        
-    return (stop - start) * 17000
-
-print(f"직결 테스트 거리: {get_dist()} cm")
+try:
+    while True:
+        dist = px.ultrasonic.read()
+        print(f"현재 측정 거리: {dist} cm", flush=True)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
